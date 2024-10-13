c75b9f642148ed1b2e8c2506a3759d461ea78252
Amâncio Chiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2024-10-13 12:12:12 +0200

    Adicionando o .env certo

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
 require('dotenv').config();
 
-
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
-// Configuração da pool de conexão
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  });
-  
-  // Teste de conexão
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Erro ao conectar à base de dados:', err.stack);
-    }
-    console.log('Conectado ao PostgreSQL!');
-  });
+// Configuração da pool de conexão para MySQL
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
-
+// Teste de conexão
+pool.getConnection((err, connection) => {
+  if (err) {
+    return console.error('Erro ao conectar à base de dados:', err.stack);
+  }
+  console.log('Conectado ao MySQL!');
+  connection.release(); // Libera a conexão
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -32,3 +30,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor a correr em http://localhost:${port}`);
 });
+
